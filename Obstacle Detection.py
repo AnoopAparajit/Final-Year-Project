59e9cbb54ef0d111246ac53069d1848962a582e9
AMOUSINF <amousinfofficial@gmail.com>
2017-05-02 17:06:55 +0530

    Obstacle Detection Commit

--- Obstacle Detection.py
+++ Obstacle Detection.py
@@ -3,10 +3,10 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3, GPIO.IN)                            #Right sensor connection
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Left sensor connection
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Left sensor connection
 while True:
           i=GPIO.input(3)                         #Reading output of right IR sensor
-          j=GPIO.input(16)                        #Reading output of left IR sensor
+          j=GPIO.input(5)                        #Reading output of left IR sensor
           if i==0:                                #Right IR sensor detects an object
                print "Obstacle detected on Left",i
                time.sleep(0.1)
